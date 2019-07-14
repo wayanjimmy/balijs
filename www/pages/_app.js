7e0f64a243199e4d6cb5defe64453461cc5c4ecd
Wayan Jimmy <jimmyeatcrab@gmail.com>
2019-07-14 14:53:07 +0700

    Initialize styled-components

---
+++ www/pages/_app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
--- www/pages/index.js
+++ www/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
-import Head from '../components/head';
-import Nav from '../components/nav';
+
+import Head from '../views/head';
+import Nav from '../views/nav';
 
 const Home = () => (
   <div>
     <Head title="Home" />
     <Nav />
-
     <div className="hero">
       <h1 className="title">Welcome to Next!</h1>
       <p className="description">
