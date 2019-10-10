df7a25ece1c7873625ae48555075fb411ecfe9f2
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2019-10-10 23:19:12 +0700

    update headline props

--- www/uikits/Text/Headline3.js
+++ www/uikits/Text/Headline3.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Headline3Styled = styled.h3``;
+const Headline3Styled = styled.h3`
+  text-align: ${props => props.align || 'auto'};
+`;
+
+/*
+props = {
+    align: 'auto' | 'left' | 'right' | 'center' | 'justify'
+}
+*/
 
 function Headline3(props) {
-  const { style } = props;
-  return <Headline3Styled style={style}>{props.children}</Headline3Styled>;
+  const { style, ...otherProps } = props;
+  return (
+    <Headline3Styled style={style} {...otherProps}>
+      {props.children}
+    </Headline3Styled>
+  );
 }
 
 export default Headline3;
