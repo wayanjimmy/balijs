70af22d89fb0cbabd7968e5c5a8b25594226f96d
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2019-10-12 13:11:21 +0700

    adding column

--- www/uikits/index.js
+++ www/uikits/index.js
@@ -1,3 +1,7 @@
+export { CommonCard } from './Card';
+
+export { default as Column, Columns } from './Commons/Column';
+
 export { Thumbnail } from './Thumbnail';
 
 export { Headline3, Paragraph } from './Text';
