0b11c724432e1b9bbc61160571b5836335c2649a
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2019-10-15 22:56:10 +0700

    update icon uikit

--- www/uikits/Icons/index.js
+++ www/uikits/Icons/index.js
@@ -1,8 +1,19 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const Icon = styled.img`
+  height: auto;
+  width: 24px;
+  margin: 0 5px;
+`;
 
 function Icons(props) {
-  const { iconName } = props;
-  return <img src={`/static/${iconName}`} />;
+  const { iconName, ext = 'svg', path } = props;
+  let src = `/static/icons/${iconName}.${ext}`;
+  if (path) {
+    src = `${path}${src}`;
+  }
+  return <Icon src={src} />;
 }
 
 export default Icons;
