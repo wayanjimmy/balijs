5fe7a9c5683169f3dd4797abb276e059f25a6f4a
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2019-10-15 22:56:52 +0700

    update export

--- www/uikits/index.js
+++ www/uikits/index.js
@@ -8,6 +8,6 @@ export { default as Icons } from './Icons';
 
 export { Thumbnail } from './Thumbnail';
 
-export { Headline2, Headline3, Paragraph } from './Text';
+export { Headline2, Headline3, Paragraph, Text } from './Text';
 
 export { SeparatorSmall } from './Separator';
