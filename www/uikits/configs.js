e47f619b760a7ce7cfc47cc31ab623f1eb22204a
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2019-10-16 10:34:56 +0700

    add FontSize config

--- www/uikits/configs.js
+++ www/uikits/configs.js
@@ -12,3 +12,13 @@ export const fontStyle = {
   openSans: `'Open Sans', sans-serif`,
   playFair: `'Playfair Display', serif`,
 };
+
+export const fontSize = {
+  small: '14px',
+  normal: '16px',
+  medium: '20px',
+  large: '40px',
+  'extra-large': '40px',
+  display: '50px',
+  'display-large': '67px',
+};
