10a4b2a04e4efc66dac4ee746f4518445e2f58a0
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2019-10-16 11:43:46 +0700

    improve EventCard

--- www/uikits/Card/EventCard.js
+++ www/uikits/Card/EventCard.js
@@ -12,34 +12,63 @@ import { colorPalette } from '../configs';
 props = {
   data: {
     title: string,
-    thumbnail: string
+    thumbnail: string, 
+    time: string,
+    place: string,
+    speaker: string
   }
 }
 */
 
 const Container = styled.div`
-  max-width: 500px;
+  max-width: 750px;
   display: flex;
   border-radius: 20px;
   box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.06);
   margin: auto;
   overflow: hidden;
+  box-shadow: 0 0 6px 3px rgba(0, 0, 0, 0.2);
 `;
 
 function EventCard(props) {
-  const { data } = props;
+  const { data = {} } = props;
   return (
     <Container>
       <View style={{ flex: 1, padding: '20px' }}>
-        <Headline3 variant="playFair">{data.title}</Headline3>
-        <View style={{ display: 'flex', alignItems: 'center' }}>
-          <Icons iconName="calendar" path="http://localhost:3000" />
-          <Text color="pencil">July 10 - 18.00 WITA</Text>
+        <View style={{ flex: 1 }}>
+          <View>
+            <Headline3 variant="playFair" size="large">
+              {data.title}
+            </Headline3>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              marginTop: '10px',
+              marginBottom: '10px',
+            }}
+          >
+            <Icons iconName="calendar" path="http://localhost:3000" />
+            <Text color="pencil" size="small">
+              {data.time}
+            </Text>
+            <Icons iconName="location-point" path="http://localhost:3000" />
+            <Text color="pencil" size="small">
+              {data.place}
+            </Text>
+          </View>
+          <View>
+            <Text color="pencil">Speaker: &nbsp;</Text>
+            <Text>{data.speaker}</Text>
+          </View>
         </View>
-        <CommonButton>Going</CommonButton>
+        <View style={{ margin: '20px 0' }}>
+          <CommonButton>Going</CommonButton>
+        </View>
       </View>
       <View style={{ flex: 1, backroundColor: colorPalette.yellow }}>
-        <Thumbnail src="https://via.placeholder.com/300/09f/fff.png" />
+        <Thumbnail src={data.thumbnail} alt={data.title} />
       </View>
     </Container>
   );
